32169903f9b742497f0a3886438f89f0384afb2f
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2017-11-06 00:37:09 -0600

    frequency rep

--- lib/wordfreq.ex
+++ lib/wordfreq.ex
@@ -10,14 +10,23 @@ defmodule WordFreq do
     |> String.split(" ")
     |> count(%{})
   end
-  defp count([], map) do
-    map
-  end
   defp count([h|t], map) do
     count(t, Map.update(map, h, 1, &(&1+1)))
   end
+  defp count([], map) do
+    map
+  end
 
-  def normalize(str) do
+  def frequency(words) do
+    cnt = count(words)
+    sum = Enum.reduce(cnt, 0, fn({_, v}, acc)->v+acc end)
+    for {k, v} <- cnt do
+      {k, v/sum}
+    end
+    |> Enum.sort_by(&(elem(&1,1)), &>=/2)
+  end
+
+  defp normalize(str) do
     Regex.replace(~r/[^\w\s]/, String.downcase(str), "")    
   end
 
